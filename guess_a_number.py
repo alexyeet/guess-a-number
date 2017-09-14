92e1fb8849be3b3666659ab1a886ae4bf3958198
Fossum <afossu4634@greenville.k12.sc.us>
2017-09-14 09:26:34 -0400

    made a guess limit

--- guess_a_number.py
+++ guess_a_number.py
@@ -4,13 +4,17 @@ import random
 
 low=1
 high=100
+limit=10
+
+#game
 
 rand = random.randint(low, high)
 print("i'm thinking of a number from "+str(low)+" to "+str(high)+".");
 
 guess = -1
+tries = 0
 
-while guess != rand:
+while guess != rand and tries < limit:
     guess = input("take a guess: ")
     guess = int(guess)
     
@@ -18,7 +22,15 @@ while guess != rand:
         print("too low")
     elif guess > rand:
         print("too high")
-    else:
-        print("good job!!!")
 
-print("yeet")
+    tries += 1
+
+if guess == rand:
+    print ("yeet u did it")
+    print ("u should be a medical terminologist")
+
+else:
+    print ("ur dumb as bricks it was actually " +str(rand))
+    print ("try taking medical terminology then try again :/")
+    
+    
