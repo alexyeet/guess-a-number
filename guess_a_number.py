29076d6d4b2e58398249b4c2961b57ddd80ca6fa
Fossum <afossu4634@greenville.k12.sc.us>
2017-09-15 09:28:30 -0400

    response to word guesses

--- guess_a_number.py
+++ guess_a_number.py
@@ -6,7 +6,19 @@ low=1
 high=100
 limit=10
 
-#game
+#translating guess to numeric
+
+def get_guess():
+    while True:
+        g = input("take a guess: ")
+        
+        if g.isnumeric():
+            g=int(g)
+            return g
+        else:
+            print ("hey dude it's gotta be a number")
+
+#game start 
 
 rand = random.randint(low, high)
 print("i'm thinking of a number from "+str(low)+" to "+str(high)+".");
@@ -15,8 +27,7 @@ guess = -1
 tries = 0
 
 while guess != rand and tries < limit:
-    guess = input("take a guess: ")
-    guess = int(guess)
+    guess = get_guess()
     
     if guess < rand:
         print("too low")
@@ -25,6 +36,8 @@ while guess != rand and tries < limit:
 
     tries += 1
 
+#game end
+    
 if guess == rand:
     print ("yeet u did it")
     print ("u should be a medical terminologist")
